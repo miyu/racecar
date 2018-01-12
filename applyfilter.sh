284657bb667a9bd1a3ceff2a492de101c6cfad3e
myu94 <myu94@bane.csenetid.cs.washington.edu>
2018-01-11 18:24:40 -0800

    fig8s

---
+++ applyfilter.sh
@@ -0,0 +1,1 @@
+roslaunch lab0 ApplyFilter.launch filter_path:=/home/myu94/catkin_ws/src/lab0/data/filter2.csv sub_topic:=/camera/color/image_raw pub_topic:=/camera/color/image_filtered fast_convolve:=false
