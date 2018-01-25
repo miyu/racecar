150cc40209c0c88ce159863bf6f4d6799102b976
hima12 <hima12@merovingian.csenetid.cs.washington.edu>
2018-01-24 16:18:11 -0800

    Motion Model Part 2

--- src/lab1/src/MotionModel.py
+++ src/lab1/src/MotionModel.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import math
 import rospy
 import numpy as np
 import utils as Utils
@@ -98,48 +99,52 @@ class OdometryMotionModel:
     
 class KinematicMotionModel:
 
-  def __init__(self, particles, state_lock=None):
+  def __init__(self, particles=None, state_lock=None):
     self.last_servo_cmd = None # The most recent servo command
     self.last_vesc_stamp = None # The time stamp from the previous vesc state msg
-    self.particles = particles
+    self.particles = particles if particles is not None else np.array([[0, 0, 0]], dtype=float)
     self.SPEED_TO_ERPM_OFFSET = float(rospy.get_param("/vesc/speed_to_erpm_offset")) # Offset conversion param from rpm to speed
     self.SPEED_TO_ERPM_GAIN   = float(rospy.get_param("/vesc/speed_to_erpm_gain"))   # Gain conversion param from rpm to speed
     self.STEERING_TO_SERVO_OFFSET = float(rospy.get_param("/vesc/steering_angle_to_servo_offset")) # Offset conversion param from servo position to steering angle
     self.STEERING_TO_SERVO_GAIN   = float(rospy.get_param("/vesc/steering_angle_to_servo_gain")) # Gain conversion param from servo position to steering angle
-    
+
     if state_lock is None:
       self.state_lock = Lock()
     else:
       self.state_lock = state_lock
-      
-    # This subscriber just caches the most recent servo position command
-    self.servo_pos_sub  = rospy.Subscriber(rospy.get_param("~servo_pos_topic", "/vesc/sensors/servo_position_command"), Float64,
-                                       self.servo_cb, queue_size=1)
-    #To get velocity
-    self.motion_vel_sub = rospy.Subscriber(rospy.get_param("~vel", "/vesc/sensors/core"), VescStateStamped
-                                           , self.motion_cb)
+
+# This subscriber just caches the most recent servo position command
+    self.servo_pos_sub  = rospy.Subscriber(rospy.get_param("~servo_pos_topic", "/vesc/sensors/servo_position_command"), Float64,self.servo_cb, queue_size=1)
+#To get velocity
+    self.motion_vel_sub = rospy.Subscriber(rospy.get_param("~vel", "/vesc/sensors/core"), VescStateStamped, self.motion_cb)
 
   def servo_cb(self, msg):
     self.last_servo_cmd = msg.data # Just update servo command
 
   def motion_cb(self, msg):
     self.state_lock.acquire()
-    
+
+# if no servo info, skip
     if self.last_servo_cmd is None:
       return
 
+  # if no prev timestamp, then play as if dt is 0
     if self.last_vesc_stamp is None:
       self.last_vesc_stamp = msg.header.stamp
+
+    dt = (msg.header.stamp - self.last_vesc_stamp).to_sec()
 
     # Convert raw msgs to controls
     # Note that control = (raw_msg_val - offset_param) / gain_param
     # YOUR CODE HERE
     
-    vel = msg.data
+    curr_speed = float(msg.state.speed - self.SPEED_TO_ERPM_OFFSET) / float(self.SPEED_TO_ERPM_GAIN)
+    curr_steering_angle = float(self.last_servo_cmd - self.STEERING_TO_SERVO_OFFSET) / float(self.STEERING_TO_SERVO_GAIN) 
 
-    print("Velocity ", vel)
-    
-    
+    print("Velocity ", curr_speed)
+    print("Steering Angle: ", curr_steering_angle) 
+    print("Delta Time: ", dt)   
+ 
     self.apply_motion_model(proposal_dist=self.particles, control=[curr_speed, curr_steering_angle, dt])
     self.last_vesc_stamp = msg.header.stamp
     self.state_lock.release()
@@ -147,8 +152,17 @@ class KinematicMotionModel:
   def apply_motion_model(self, proposal_dist, control):
     # Update the proposal distribution by applying the control to each particle
     # YOUR CODE HERE
-    pass
-    
+    [curr_speed, curr_steering_angle, dt] = control
+    for i in range(len(proposal_dist)):
+      [x,y,theta] = proposal_dist[i]
+      dx = (curr_speed) * math.cos(theta)
+      dy = (curr_speed) * math.sin(theta)
+      beta = math.atan(math.tan(curr_steering_angle) / 2)
+      dtheta = ((curr_speed) / 0.33) * math.sin(2*beta) # Length = 0.33 m
+      self.particles[i] = proposal_dist[i] + np.array([dx, dy, dtheta], dtype=float) * dt
+      print(self.particles[i])
+
+
 if __name__ == '__main__':
   rospy.init_node('OdoTest1', anonymous=True)
   #Odo = OdometryMotionModel(None, None)
