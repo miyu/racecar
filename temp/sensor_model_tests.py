e5876d14bf775e1e874bcfef422a5ca6d669f098
myu94 <myu94@bane.csenetid.cs.washington.edu>
2018-01-27 13:54:38 -0800

    Sensor model graph

--- temp/sensor_model_tests.py
+++ temp/sensor_model_tests.py
@@ -1,21 +1,40 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 
+# Z_SHORT = 0.25  # Weight for short reading
+# Z_MAX = 0.25    # Weight for max reading
+# Z_RAND = 0.25   # Weight for random reading
+# SIGMA_HIT = 0.2 # Noise value for hit reading
+# Z_HIT = 0.25    # Weight for hit reading
+
+Z_HIT = 4
+Z_SHORT = 3
+Z_MAX = 0.1
+Z_RAND = 2
+
+Z_NORM = float(Z_HIT + Z_SHORT + Z_MAX + Z_RAND)
+Z_HIT /= Z_NORM
+Z_SHORT /= Z_NORM
+Z_MAX /= Z_NORM
+Z_RAND /= Z_NORM
+
+
 THETA_DISCRETIZATION = 112 # Discretization of scanning angle
-INV_SQUASH_FACTOR = 2.2    # Factor for helping the weight distribution to be less peaked
+SQUASH_FACTOR_N = 1.5
+INV_SQUASH_FACTOR = 1 / SQUASH_FACTOR_N    # Factor for helping the weight distribution to be less peaked
 
-Z_SHORT = 0.25  # Weight for short reading
-Z_MAX = 0.25    # Weight for max reading
-Z_RAND = 0.25   # Weight for random reading
-SIGMA_HIT = 0.2 # Noise value for hit reading
-Z_HIT = 0.25    # Weight for hit reading
+# Z_SHORT = 0.25  # Weight for short reading, something in the way
+# Z_MAX = 0.25    # Weight for max reading, out of range
+# Z_RAND = 0.25   # Weight for random reading, random noise
+# Z_HIT = 0.25    # Weight for hit reading, hit obj properly
+# Z_SHORT = 0.1  # Weight for short reading, something in the way
+# Z_MAX = 0.1    # Weight for max reading, out of range
+# Z_RAND = 0.1   # Weight for random reading, random noise
+# Z_HIT = 0.7    # Weight for hit reMAX_RANGE_METERSading, hit obj properly
+SIGMA_HIT = 20 # Noise value for hit reading
 
-Z_SHORT = 1
-Z_MAX = 0
-Z_RAND = 0
-Z_HIT = 0
-
-LAMBDA_SHORT = 3.0
+LAMBDA_SHORT = 0.01
 
 def precompute_sensor_model(max_range_px):
     table_width = int(max_range_px) + 1
@@ -28,7 +47,7 @@ def precompute_sensor_model(max_range_px):
     # Note that the row corresponds to the observed measurement and the column corresponds to the expected measurement
     # YOUR CODE HERE
 
-    assert Z_HIT + Z_SHORT + Z_MAX + Z_RAND == 1.0
+    assert abs((Z_HIT + Z_SHORT + Z_MAX + Z_RAND) - 1.0) < 1E-4
 
     def compute_eng(ztk, ztkstar, varhit):
         denom1 = math.sqrt(2.0 * np.pi * varhit)
@@ -36,23 +55,28 @@ def precompute_sensor_model(max_range_px):
         denom2 = varhit
         return (1.0 / denom1) * np.exp((-0.5) * numer2 / denom2)
 
+
+    def compute_p_short_unnormalized(ztk, ztkstar):
+        if 0.0 <= ztk <= ztkstar:
+            return LAMBDA_SHORT * np.exp(-LAMBDA_SHORT * ztk)
+        else:
+            return 0.0
+
     engs = np.zeros((table_width, table_width))
+    p_short_unnormalized = np.zeros((table_width, table_width))
     for i in range(table_width):
         for j in range(table_width):
             engs[i][j] = compute_eng(float(i), float(j), SIGMA_HIT * SIGMA_HIT)
+            p_short_unnormalized[i][j] = compute_p_short_unnormalized(float(i), float(j))
 
     engs = engs / engs.sum(axis=0, dtype=float)
+    p_short_normalized = p_short_unnormalized / p_short_unnormalized.sum(axis=0, dtype=float)
 
     def compute_p_hit(i, j, max_range_px):
         return engs[i][j] if 0.0 <= i <= max_range_px else 0.0
 
     def compute_p_short(i, j):
-        if j == 0:
-            return 1.0 if i == 0 else 0.0
-        if not (0 <= i <= j):
-            return 0.0
-        p_short_eng = 1.0 / (1.0 - np.exp(-LAMBDA_SHORT * j))
-        return p_short_eng * LAMBDA_SHORT * np.exp(-LAMBDA_SHORT * i)
+        return p_short_normalized[i][j]
 
     def compute_p_max(i, max_range_px):
         return 1 if i == max_range_px else 0.0
@@ -67,22 +91,31 @@ def precompute_sensor_model(max_range_px):
             p_max = compute_p_max(i, max_range_px)
             p_rand = compute_p_rand(i, max_range_px)
 
-            print(i, j, max_range_px, LAMBDA_SHORT, p_hit, p_short, p_max, p_rand)
+            #print(i, j, max_range_px, LAMBDA_SHORT, p_hit, p_short, p_max, p_rand)
 
             assert 0 <= p_hit <= 1
             assert 0 <= p_rand <= 1
             assert 0 <= p_short <= 1
             assert 0 <= p_max <= 1
 
-            sensor_model_table[i][j] = Z_HIT * p_hit + Z_SHORT * p_short + Z_MAX * p_max + Z_RAND * p_rand
+            sensor_model_table[i][j] = Z_HIT*p_hit + Z_SHORT*p_short + Z_MAX*p_max + Z_RAND*p_rand
 
     column_sums = sensor_model_table.sum(axis=0)
-    print(column_sums)
-    #for j in range(table_width):
-    #    assert abs(column_sums[j] - 1.0) <= 1E-4
+    row_sums = sensor_model_table.sum(axis=1)
+    #print("Column Sums: ", column_sums)
+
+    print("SMT:")
+    print(sensor_model_table)
+    for j in range(table_width):
+        assert abs(column_sums[j] - 1.0) <= 0.02
 
     return sensor_model_table
 
-sm = precompute_sensor_model(7)
-print(sm)
-print(sm.sum(axis=0))
+sm = precompute_sensor_model(280)
+print("Sum on columns: ", sm.sum(axis=0))
+model_size = sm.shape[0]
+xs = range(0, model_size)
+ys = sm[xs, int(model_size * 3) / 5]
+#print(ys)
+plt.plot(xs, ys, '.')
+plt.show()
