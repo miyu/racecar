174485733c43ef2a382e76894b2759debdb92cc0
myu94 <myu94@bane.csenetid.cs.washington.edu>
2018-01-27 16:22:52 -0800

    comment bnechmark' git

--- src/lab1/src/Debug.py
+++ src/lab1/src/Debug.py
@@ -3,4 +3,5 @@ def print_locks(*args):
     pass
 
 def print_benchmark(what, start_time, end_time):
-    print("benchmark: ", what, "took", (end_time - start_time) * 1000, "millis")
+    #print("benchmark: ", what, "took", (end_time - start_time) * 1000, "millis")
+    pass
