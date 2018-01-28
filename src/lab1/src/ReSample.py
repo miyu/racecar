e4b1726f7ec2e2332930f1a5029e2a9c5dd3a383
ANANTH SRINIVAS GOTTUMUKKALA <ananthgo@cs.washington.edu>
2018-01-27 23:09:28 -0800

    Added low variance sampling (didn't test)

--- src/lab1/src/ReSample.py
+++ src/lab1/src/ReSample.py
@@ -45,6 +45,23 @@ class ReSampler:
     self.state_lock.acquire()
     print_locks("Entered lock resample_low_variance")
     start_time = time.time()
+    
+    M = len(self.particles)
+    new_particles = np.array(self.particles.shape, dtype=np.float64)
+    r = random.uniform(0, 1.0 / M) # Draw random number in the range [0, 1/M]
+    U = r - (1.0 / M)
+    particle_index = 0
+    weight_total = self.weights[0]
+    
+    for new_particle_index in range(M):
+        U += (1.0 / M)
+        while U > weight_total:
+            particle_index += 1
+            weight_total += self.weights[particle_index % M]
+        particle_index = particle_index % M
+        new_particles[new_particle_index] = self.particles[particle_index]
+        
+    self.particles = new_particles
 
     print_locks("Exiting lock resample_low_variance")
     print_benchmark("resample_low_variance", start_time, time.time())
