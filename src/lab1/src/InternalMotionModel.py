Fixed Low Variance Sampler?

src/lab1/src/InternalMotionModel.py
@@ -66,7 +66,7 @@ class InternalOdometryMotionModel:
 class InternalKinematicMotionModel:
     def __init__(self, particles, noise_params=None):
         self.particles = particles
-        self.noise_params = np_array_or(noise_params, np.array([[0.1, 0.03], [0.1, 0.03]]))
+        self.noise_params = np_array_or(noise_params, np.array([[0.1, 0.1], [0.1, 0.3]]))
 
     def update(self, control):
         num_particles = self.particles.shape[0]

src/lab1/src/ReSample.py
@@ -47,11 +47,11 @@ class ReSampler:
     start_time = time.time()
     
     M = len(self.particles)
-    new_particles = np.array(self.particles.shape, dtype=np.float64)
     r = np.random.uniform(0, 1.0 / M) # Draw random number in the range [0, 1/M]
     U = r - (1.0 / M)
     particle_index = 0
     weight_total = self.weights[0]
+    self.particle_indices = np.zeros(M, dtype=int)
     
     for new_particle_index in range(M):
         U += (1.0 / M)
@@ -59,10 +59,10 @@ class ReSampler:
             particle_index += 1
             weight_total += self.weights[particle_index % M]
         particle_index = particle_index % M
-        print(new_particles[new_particle_index][:].shape, self.particles[particle_index][:].shape)
-        new_particles[new_particle_index][:] = self.particles[particle_index][:]
+        self.particle_indices[new_particle_index] = particle_index
         
-    self.particles = new_particles
+    self.particles[:, :] = self.particles[self.particle_indices, :]
+    self.weights[:] = (1.0 / M)
 
     print_locks("Exiting lock resample_low_variance")
     print_benchmark("resample_low_variance", start_time, time.time())