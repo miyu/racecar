46a28f922584e1a3e9cb9c9a301fc8a929435ba2
hima12 <hima12@choi.csenetid.cs.washington.edu>
2018-01-29 19:58:36 -0800

    Able to plot the discretization vs time

--- src/lab1/src/ParticleLikelihood.py
+++ src/lab1/src/ParticleLikelihood.py
@@ -2,11 +2,13 @@
 
 import rospy
 import rosbag
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from threading import Lock
 
 import matplotlib.colors as colors
+import matplotlib.cm as cm
 from std_msgs.msg import Int32, String
 from SensorModel import SensorModel
 from nav_msgs.srv import GetMap
@@ -15,76 +17,88 @@ from nav_msgs.srv import GetMap
 
 
 class ParticleLikelihood():
-    def __init__(self):
-        BAG_FILENAME = str(rospy.get_param("~bag_file"))
-        bag = rosbag.Bag(BAG_FILENAME)
-        message = None
-        try:
-            for topic, msg, t in bag.read_messages():
-                message = msg
-        finally:
-            bag.close()
+    """
+        Initalize the ParticleLikelihood, by getting the bag and message
 
+        Input:
+            message: The message from the bag file
+
+    """
+    def __init__(self, message):
         #print(message.ranges)
+        self.message = message
 
+        # Need to run the map server to get the map
         rospy.wait_for_service('/static_map')
         get_map = rospy.ServiceProxy('/static_map', GetMap)
-        map_msg = get_map().map
+        self.map_msg = get_map().map
 
-        print(map_msg.header)
-        print(len(map_msg.data))
+        #print("Map msg header: ",map_msg.header)
+        #print("length of map msg data: ", len(map_msg.data))
+        #print("Map msg info: ", map_msg.info)
 
-        print(map_msg.info)
+        self.width = self.map_msg.info.width
+        #print("Width: ", width)
 
-        width = map_msg.info.width
-        print("Width: ", width)
+        self.height = self.map_msg.info.height
+        #print("Height: ", height)
 
-        height = map_msg.info.height
-        print("Height: ", height)
+        self.resolution = self.map_msg.info.resolution
+        #print("Resolution: ", resolution )
 
-        resolution = map_msg.info.resolution
-        print("Resolution: ", resolution )
+        self.origin = self.map_msg.info.origin.position
+        #print("Origin: ", origin )
 
-        origin = map_msg.info.origin.position
-        print("Origin: ", origin )
+    """
+        Discretize the map and get the particle. Then apply the sensor MODEL
+        to get the particle likelihood.
 
+        Input:
+            meter_gap: the distance between each particle in meter
+            theta_step_size: The number of orientation for each x,y position
+            plot_likelihood: Plot the particle likelihood?
+        Output:
+            The time it took to discretize and observe the map
+
+    """
+    def apply_sensor_model(self,meter_gap=.02, theta_step_size=10,plot_likelihood = True):
+        start = time.time()
 
         particles = []
         max_particles = 0
 
-        theta_step_size = 10    # The number of different orientation
-        meter_var = .02         # The discretization of the map in meter
-        cell_var = int(meter_var/resolution)
+        #theta_step_size = 10    # The number of different orientation
+        #meter_gap = .02         # The discretization of the map in meter
+        cell_gap = int(meter_gap/self.resolution)
 
-        for i in range(0,height, cell_var):
-            for j in range(0,width, cell_var):
-                if(map_msg.data[i*height+j] != -1):
+        # Discretize the map with spceified meter
+        for i in range(0,self.height, cell_gap):
+            for j in range(0,self.width, cell_gap):
+                if(self.map_msg.data[i*self.height+j] != -1):
                     # Place a particle in all/some x,y valid position
                     #print("Known space: (", i, ", " ,j , ") with value ",
                     #      map_msg.data[i*height+j])
-                    x = j * resolution + origin.x
-                    y = i * resolution + origin.y
+                    x = j * self.resolution + self.origin.x
+                    y = i * self.resolution + self.origin.y
                     # Iterate through some theta.
                     for theta in range(1, theta_step_size+1):
                         particles.append([x, y, np.pi*2 * theta / theta_step_size])
                         max_particles += 1
-
-        print("Done getting particles")
+        #print("Done getting particles")
         weights = np.ones(max_particles, dtype=np.float64) / max_particles
         np_particles = np.array(particles, dtype=np.float64)
 
-        print("Old weights ",weights)
-        print("Old position ",np_particles)
-        print("Maximum particles ",max_particles)
+        #print("Old weights ",weights)
+        #print("Old position ",np_particles)
+        #print("Maximum particles ",max_particles)
 
-        state_lock = Lock()
-        sensorModel = SensorModel(map_msg, np_particles, weights )
-        sensorModel.lidar_cb(message)
+        sensorModel = SensorModel(self.map_msg, np_particles, weights )
+        sensorModel.lidar_cb(self.message)
         new_particles = sensorModel.particles
         new_weights = sensorModel.weights
 
-        print("New weights ", new_weights)
-        print("New particles ", new_particles)
+        #print("New weights ", new_weights)
+        #print("New particles ", new_particles)
 
         #x = new_particles[:,0]
         #y = new_particles[:,1]
@@ -104,27 +118,59 @@ class ParticleLikelihood():
         max_particle = np.array(max_particle)
         max_weight = np.array(max_weight)
 
-        #max_weight *= (1/np.amax(max_weight))
+        #print("Max particle:", max_particle)
+        #print("Max weight:", max_weight)
 
-
-
-        print("Max particle:", max_particle)
-        print("Max weight:", max_weight)
-
-        print("Max particle len: ", len(max_particle))
-        print("Max weight len: ", len(max_weight))
+        #print("Max particle len: ", len(max_particle))
+        #print("Max weight len: ", len(max_weight))
         x = max_particle[:, 0]
         y = max_particle[:, 1]
 
-        print("The largest weight: ", np.amax(max_weight))
-        print("The smallest weight: ", np.amin(max_weight))
+        np.amax(max_weight)
+        #print("The largest weight: ", np.amax(max_weight))
+        #print("The smallest weight: ", np.amin(max_weight))
 
-        plt.scatter(x, y, c=max_weight,edgecolor='')
-        plt.gray()
-        plt.show()
+        end = time.time()
 
-        print("Got to update sensor model")
+        if plot_likelihood:
+            plt.scatter(x, y, c=max_weight, vmin = np.amin(max_weight), vmax = np.amax(max_weight),cmap=cm.get_cmap('cool'),edgecolor='')
+            plt.gray()
+            plt.show()
+
+        #print("Got to update sensor model")
+
+        return end - start
 
 if __name__=='__main__':
     rospy.init_node("particle_likelihood", anonymous=True)
-    pl = ParticleLikelihood()
+    BAG_FILENAME = str(rospy.get_param("~bag_file"))
+    bag = rosbag.Bag(BAG_FILENAME)
+    message = None
+    try:
+        for topic, msg, t in bag.read_messages():
+            message = msg
+    finally:
+        bag.close()
+    pl = ParticleLikelihood(message)
+
+    pl.apply_sensor_model(.02)
+
+    """
+    MIN_METER = .02
+    MAX_METER = .5
+    METER_STEP = .02
+    time_list = []
+    meter_dis = []
+
+    for meter in np.arange(MIN_METER, MAX_METER, METER_STEP):
+        delta_time = pl.apply_sensor_model(meter, plot_likelihood = F alse)
+        time_list.append(delta_time)
+        meter_dis.append(meter)
+        print('Completed discretization with meter = ', meter)
+        print('Time it took to complete is ', delta_time ,' sec')
+    plt.plot(meter_dis, time_list)
+    plt.xlabel("Distance between each particle (meter)")
+    plt.ylabel("Time (sec)")
+    plt.title("Time vs Discretization")
+    plt.show()
+    """
